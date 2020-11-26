9c1f21818eca8c3bc44d9db8a0f924a33e4e5794
SebastianAvila <jesusaviala@gmail.com>
2020-11-26 16:44:26 -0600

    Se creo la bankend de los vatos

--- Prueba.php
+++ Prueba.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<title> Registro de usuario </title> 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
     <title>Prueba para subir a GitHub</title>
     
 </head>
 <body>
+
+<form method "post"> 
+<h1>  Suscribete <h1> 
+<input type="text" name="name" placeholder="Nombre Completo">
+<input type="email" name="email" >
+<input type="submit" name="register">
+
+</form> 
+
+<? php
+include("coneccionBasedatos.php");
+
+
+
+
     
 </body>
 </html>
