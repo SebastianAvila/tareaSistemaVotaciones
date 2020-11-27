2b03c1162bb1d9d90c00aceaae18676898e6994b
SebastianAvila <jesusaviala@gmail.com>
2020-11-26 20:50:53 -0600

    se agrego los votones para votar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de registo de Votos</title>
+</head>
+<body>
+
+
+<h3 class="normalFont">Seleccina uno de los siguientes:</h3>
+            <div class="radio">
+              <label for="">
+                <input type="radio" name="selectedCandidate" value="candidato1"> <strong>candidato 1 </strong>
+              </label><br>
+
+              <label for="">
+                <input type="radio" name="selectedCandidate" value="candicato2"> <strong>candidato 2</strong> 
+              </label><br>
+              
+              <label for="">
+                <input type="radio" name="selectedCandidate" value="candicato2"> <strong>Candidato 3 </strong>
+              </label><br>
+              <hr>
+              <button type="submit" name="submit" class="btn btn-primary"><strong>Enviar</strong></button>
+              <button type="submit" class="btn btn-default">Declinar</button>
+            </div>
+          </div>
+
+
+    
+</body>
+</html>
