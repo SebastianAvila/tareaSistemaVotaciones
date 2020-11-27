5f30002713ba2082e7c03aea6f88deaa18d1b668
SebastianAvila <jesusaviala@gmail.com>
2020-11-26 21:21:16 -0600

    se agrego la logica del voto

--- index.php
+++ index.php
@@ -11,15 +11,15 @@
 <h3 class="normalFont">Seleccina uno de los siguientes:</h3>
             <div class="radio">
               <label for="">
-                <input type="radio" name="selectedCandidate" value="candidato1"> <strong>candidato 1 </strong>
+                <input type="radio" name="selecioncandidato" value="candidato1"> <strong>candidato 1 </strong>
               </label><br>
 
               <label for="">
-                <input type="radio" name="selectedCandidate" value="candicato2"> <strong>candidato 2</strong> 
+                <input type="radio" name="selecioncandidato" value="candicato2"> <strong>candidato 2</strong> 
               </label><br>
               
               <label for="">
-                <input type="radio" name="selectedCandidate" value="candicato2"> <strong>Candidato 3 </strong>
+                <input type="radio" name="selecioncandidato" value="candicato2"> <strong>Candidato 3 </strong>
               </label><br>
               <hr>
               <button type="submit" name="submit" class="btn btn-primary"><strong>Enviar</strong></button>
