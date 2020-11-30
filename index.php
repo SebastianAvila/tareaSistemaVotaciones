9b740237dcdef1a4df36aa9c792fb2a00c2ba8b6
SebastianAvila <jesusaviala@gmail.com>
2020-11-29 23:32:07 -0600

    Se dio estilo a indez, mañana a admin

--- index.php
+++ index.php
@@ -1,41 +1,42 @@
 <html>
 <head>   
 <!--- Titulo puedes cambiarlo  --> 
-<title> Candidatos 2022 </title> 
+<title class="specialHead" > Candidatos 2022 </title> 
 </head>
 
 <body>
-    <h1> Seleccione Al Candidato De Su Preferencia</h1> 
+    <h1 class="specialHead"> Sistema de votos </h1> 
+    <h1 class="specialHead"> Seleccione Al Candidato De Su Preferencia</h1> 
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 
     <!--  Aqui esta el vinculo, solo cambia este nombre, por el del css que ocupes  -->
-    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <!--<link rel="stylesheet" type="text/css" href="estilos.css">--> 
+    <link rel="stylesheet" type="text/css" href="estilosIndex.css">
         <div class ="container"> 
         <!--- No cambies esto, es el metodo de envio  --> 
         <form action="" method="POST" > 
-            <label> Nombre </label> <br>
+            <label class="specialHead"> Nombre </label> <br>
             <input type="text" name="username" placeholder="Ingresa tu nombre"/> <br>
             <!--- radio buttoms aqui importa el "name" y el "value" es el que se manda a la base de datos  --> 
-            <label> Seleccione La Opcion Que Mas Le Convenga </label> <p>
-            <input type="radio" name="opcion" value="PRIN"/> PRIN
-            <input type="radio" name="opcion" value="PANA"/> PANA
-            <input type="radio" name="opcion" value="PRANDA"/> PRANDA
-            <input type="radio" name="opcion" value="PARTIDO AZUL"/> PARTIDO AZUL            
-            <input type="submit" name="insert" value="Enviar"/> </p>
+            <label class="specialHead"> Seleccione La Opcion Que Mas Le Convenga </label> <p>
+
+            <label for="PRIM" class="specialHead"> PRIM </label>
+            <input class="radio" id="PRIM" type="radio" name="opcion" value="PRIN"/> 
+            <label for="PANA" class="specialHead"> PANA </label>
+            <input class="radio" id="PANA" type="radio" name="opcion" value="PANA"/> 
+            <label for="PRANDA" class="specialHead"> PRANDA </label>
+            <input class="radio" id="PRANDA" type="radio" name="opcion" value="PRANDA"/> 
+            <label for="PARTIDO AZUL" class="specialHead"> PARTIDO AZUL  </label>
+            <input class="radio" id="PARTIDO AZUL" type="radio" name="opcion" value="PARTIDO AZUL"/>
+            
+            <input   type="submit" name="insert" value="Enviar"/> 
             
         </form>
         </div>
         
-        <div class="collapse navbar-collapse" id="example-nav-collapse">
-          <ul class="nav navbar-nav">
+        <div class="barrasuperior" id="example-nav-collapse"> 
+            <li><a href="admin.html"><strong>Panel Administrativo </strong></span></a></li>         
             
-            <li><a href="caracteristicasProgramaVotos.html"><strong>Características</strong></span></a></li>
-            <li><a href="programadores.html"><strong>Nosotros</strong></span></a></li>
-            
-        	
-          </ul>
-          
-
-          <a href="admin.html" ><strong>Panel Administrativo</strong></a></span>
         </div>
 
         
